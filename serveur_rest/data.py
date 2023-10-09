586651df226172c690f25ba94268e4b1820f98ab
Andy <andy.dubigny@gmail.com>
2023-10-09 16:13:40 +0200

    Correction 4eme requete

--- serveur_rest/data.py
+++ serveur_rest/data.py
@@ -28,7 +28,13 @@ class DataAccess :
 
     @classmethod
     def top_10(cls, grade):
-        rows = cls.session.execute('SELECT * FROM inspection WHERE grade = %s LIMIT 10', [grade])
-        return list(rows)
+        rows = cls.session.execute('SELECT idrestaurant FROM inspection WHERE grade = %s', [grade])
+        idrestaurants = list(set(row.idrestaurant for row in rows))[:10]
+        names = []
+        for idrestaurant in idrestaurants:
+            rows = cls.session.execute(f"SELECT name FROM restaurant WHERE id = {idrestaurant}")
+            for row in rows:
+                names.append(row.name)
+        return names
 
 
